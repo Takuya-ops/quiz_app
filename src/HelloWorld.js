a05e010d9b381d76732d4302546fd5698ff393cb
Takuya-ops <hora2112.ipu@gmail.com>
2023-11-23 02:19:25 +0900

    usestate practice

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,12 +1,40 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import "./HelloWorld.css";
 
 function HelloWorld(props) {
+  const [isTrue, setIsTrue] = useState(true)
+  
+const toggleTrue = () => {
+  if (isTrue) {
+    setIsTrue(false)
+    return
+  } else {
+  return
+  }
+}
+
   return (
     <Fragment>
       <hr/>
       <h1 className="h1-green">{props.msg}</h1>
       <hr/>
+
+      {/* {isTrue &&
+        <Fragment>
+          <p>登録しますか？</p>
+          <hr/>
+        </Fragment>
+      }
+      {!isTrue &&
+        <Fragment>
+          <p>登録しました。</p>
+          <hr/>
+        </Fragment>
+      } */}
+
+      {/* 上のコメントアウトのコードを１行で書くと以下のようになる。 */}
+      {isTrue ? <p>登録しますか？</p> : <p>登録しました。</p>}
+      <a href="#!" className="btn btn-outline-secondary" onClick={toggleTrue}>登録</a>
     </Fragment>
   )
 }
