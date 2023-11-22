fe2304bf37e99a2c8e89f7ceb7b1dec7c8c39a08
Takuya-ops <hora2112.ipu@gmail.com>
2023-11-23 01:45:45 +0900

    text msg change from hard_coding to props

---
+++ src/HelloWorld.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import "./HelloWorld.css";
+
+function HelloWorld(props) {
+  return (
+    <Fragment>
+      <hr/>
+      <h1 className="h1-green">{props.msg}</h1>
+      <hr/>
+    </Fragment>
+  )
+}
+
+export default HelloWorld;
